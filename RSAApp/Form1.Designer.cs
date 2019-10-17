89c0094e9d0dd15bccd1b5261e1bd6e56f1c5ad9
Лоскутов Руслан Вадимович <rasl2222@icloud.com>
2019-10-17 13:53:25 +0500

    Class creation, form gets items.

--- RSAApp/Form1.Designer.cs
+++ RSAApp/Form1.Designer.cs
@@ -28,13 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.параметрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.печатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьВExcelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.настройкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.видToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьУпражнениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изменитьУпражнениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьУпражнениеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.результатыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьАтлетаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.изменитьАтлетаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьАтлетаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControl1.HotTrack = true;
+            this.tabControl1.Location = new System.Drawing.Point(0, 24);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 426);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 393);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "РЕЗУЛЬТАТЫ";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.tabControl2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 33);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabPage2.Size = new System.Drawing.Size(792, 389);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "УПРАЖНЕНИЯ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.параметрыToolStripMenuItem,
+            this.результатыToolStripMenuItem,
+            this.видToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem,
+            this.печатьToolStripMenuItem,
+            this.сохранитьВExcelToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // параметрыToolStripMenuItem
+            // 
+            this.параметрыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.настройкиToolStripMenuItem});
+            this.параметрыToolStripMenuItem.Name = "параметрыToolStripMenuItem";
+            this.параметрыToolStripMenuItem.Size = new System.Drawing.Size(83, 20);
+            this.параметрыToolStripMenuItem.Text = "Параметры";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            // 
+            // печатьToolStripMenuItem
+            // 
+            this.печатьToolStripMenuItem.Name = "печатьToolStripMenuItem";
+            this.печатьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.печатьToolStripMenuItem.Text = "Печать";
+            // 
+            // сохранитьВExcelToolStripMenuItem
+            // 
+            this.сохранитьВExcelToolStripMenuItem.Name = "сохранитьВExcelToolStripMenuItem";
+            this.сохранитьВExcelToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьВExcelToolStripMenuItem.Text = "Сохранить в Excel";
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tabControl2.Location = new System.Drawing.Point(3, 3);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(786, 383);
+            this.tabControl2.TabIndex = 0;
+            // 
+            // настройкиToolStripMenuItem
+            // 
+            this.настройкиToolStripMenuItem.Name = "настройкиToolStripMenuItem";
+            this.настройкиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.настройкиToolStripMenuItem.Text = "Настройки";
+            // 
+            // видToolStripMenuItem
+            // 
+            this.видToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьУпражнениеToolStripMenuItem,
+            this.изменитьУпражнениеToolStripMenuItem,
+            this.удалитьУпражнениеToolStripMenuItem});
+            this.видToolStripMenuItem.Name = "видToolStripMenuItem";
+            this.видToolStripMenuItem.Size = new System.Drawing.Size(88, 20);
+            this.видToolStripMenuItem.Text = "Упражнения";
+            // 
+            // добавитьУпражнениеToolStripMenuItem
+            // 
+            this.добавитьУпражнениеToolStripMenuItem.Name = "добавитьУпражнениеToolStripMenuItem";
+            this.добавитьУпражнениеToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.добавитьУпражнениеToolStripMenuItem.Text = "Добавить упражнение";
+            // 
+            // изменитьУпражнениеToolStripMenuItem
+            // 
+            this.изменитьУпражнениеToolStripMenuItem.Name = "изменитьУпражнениеToolStripMenuItem";
+            this.изменитьУпражнениеToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.изменитьУпражнениеToolStripMenuItem.Text = "Изменить упражнение";
+            // 
+            // удалитьУпражнениеToolStripMenuItem
+            // 
+            this.удалитьУпражнениеToolStripMenuItem.Name = "удалитьУпражнениеToolStripMenuItem";
+            this.удалитьУпражнениеToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.удалитьУпражнениеToolStripMenuItem.Text = "Удалить упражнение";
+            // 
+            // результатыToolStripMenuItem
+            // 
+            this.результатыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьАтлетаToolStripMenuItem,
+            this.изменитьАтлетаToolStripMenuItem,
+            this.удалитьАтлетаToolStripMenuItem});
+            this.результатыToolStripMenuItem.Name = "результатыToolStripMenuItem";
+            this.результатыToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.результатыToolStripMenuItem.Text = "Результаты";
+            // 
+            // добавитьАтлетаToolStripMenuItem
+            // 
+            this.добавитьАтлетаToolStripMenuItem.Name = "добавитьАтлетаToolStripMenuItem";
+            this.добавитьАтлетаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.добавитьАтлетаToolStripMenuItem.Text = "Добавить атлета";
+            // 
+            // изменитьАтлетаToolStripMenuItem
+            // 
+            this.изменитьАтлетаToolStripMenuItem.Name = "изменитьАтлетаToolStripMenuItem";
+            this.изменитьАтлетаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.изменитьАтлетаToolStripMenuItem.Text = "Изменить атлета";
+            // 
+            // удалитьАтлетаToolStripMenuItem
+            // 
+            this.удалитьАтлетаToolStripMenuItem.Name = "удалитьАтлетаToolStripMenuItem";
+            this.удалитьАтлетаToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.удалитьАтлетаToolStripMenuItem.Text = "Удалить атлета";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem печатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьВExcelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem параметрыToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.ToolStripMenuItem настройкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem результатыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьАтлетаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem изменитьАтлетаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьАтлетаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem видToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem добавитьУпражнениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem изменитьУпражнениеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьУпражнениеToolStripMenuItem;
     }
 }
 
---
+++ RSAApp/results.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAApp
+{
+    class results
+    {
+        Dictionary<int, string> athlets;
+        public Dictionary<int, string> Athlets
+        {
+            get
+            {
+                return athlets;
+            }
+            set
+            {
+                bool isok = true;
+                foreach (KeyValuePair<int, string> item in value)
+                    if (item.Key <= 0||item.Key>value.Count)
+                    {
+                        isok = false;
+                        throw new Exception("Ошибка ввода! Номер атлета должен быть больше Ноля и числа участников!");
+                    }
+                foreach (KeyValuePair<int, string> item in value)
+                {
+                    
+                }
+            }
+        }
+    }
+}
